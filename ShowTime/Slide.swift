85705d83e18aee5bcd4c4e2b8e0f864c995e1816
Ivan Sebastian <ivansedbert@gmail.com>
2019-07-10 12:15:00 +0700

    Tutorial Page

---
+++ ShowTime/Slide.swift
@@ -0,0 +1,25 @@
+//
+//  Slide.swift
+//  ShowTime
+//
+//  Created by Ivan Sebastian on 10/07/19.
+//  Copyright © 2019 whiteHat. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    @IBOutlet weak var labelDesc: UILabel!
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+}
